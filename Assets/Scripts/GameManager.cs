f8f71bee00387a0f00daa397cbd6e27454f0465e
kc10@williams.edu <kc10@williams.edu>
2016-09-27 17:11:25 -0400

    Added Player, GameManager and Card classes.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance; //A static member of GameManager that can be accessed by any other member in the code.
+    private BoardManager boardGen;
+
+    // Use this for initialization
+    void Awake () {
+        // Make sure only one instance of GameManager exists.
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        //TODO: Create an instance of Board Manager.
+
+        InitGame();
+	}
+
+    void InitGame() {
+        //TODO: Make Board Manager do something.
+    }
+
+    void Update()
+    {
+
+    }
+	
+	// Update is called once per frame
+}
